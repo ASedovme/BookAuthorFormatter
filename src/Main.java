94d4472822a1bc610a021528c3f419b779ff4d24
Aleksei <alexejsedow@gmail.com>
2025-05-04 17:15:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String[][] booksAndAuthors ={
+                {"Война и Мир", "Лев Толстой"},
+                {"1984", "Джордж Оруэлл"},
+                {"Мастер и Маргарита", "Михаил Булгаков"}
+        };
+
+        //Форматируем и выводим каждую пару
+
+        for(String[] pair : booksAndAuthors){
+            String bookTitle = pair[0];
+            String authorFullName = pair[0];
+            String formattedString = formatBookAuthor(bookTitle, authorFullName);
+            System.out.println(formattedString);
+        }
+        
+
+    }
+
+    private static String formatBookAuthor(String bookTitle, String authorFullName) {
+            return "\"" + bookTitle + "\" " + authorFullName;
+    }
+}
